b46f1207a33ba51f28f3441bee73c44a8027a938
Fred Ross <madhadron@gmail.com>
2011-11-15 10:43:30 -0800

    Added build system

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='pydaemonize',
+      version='0.1',
+      description='Utilities for constructing system daemons',
+      author='Frederick Ross',
+      author_email='madhadron@gmail.com',
+      url='http://github.com/madhadron/pydaemonize',
+      packages=['pydaemonize'],
+)
